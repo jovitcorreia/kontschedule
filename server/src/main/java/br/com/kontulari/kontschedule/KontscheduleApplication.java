c1e15573388d972cc5996544693cee24c5fc3f3e
castanho <castanhocorreia.dev@gmail.com>
2021-10-08 21:52:36 -0300

    Adiciona o diretório do Spring Boot ao projeto

---
+++ server/src/main/java/br/com/kontulari/kontschedule/KontscheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.kontulari.kontschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontscheduleApplication.class, args);
+	}
+
+}
