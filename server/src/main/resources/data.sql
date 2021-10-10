143601eebf1a9b05d3c5b4f1bc4b83ef0816d42b
castanho <castanhocorreia.dev@gmail.com>
2021-10-10 06:34:50 -0300

    Remove arquivo .sql estático

--- server/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into contador (cpf, crc, nome, sobrenome, endereco)
-values ('023.825.928-50', 'BA-021871', 'Fulano', 'de Tal', 'Av. Tancredo Neves, 620, Salvador, BA, Brasil');
-
-insert into empresa (cnpj, endereco, nome_fantasia, razao_social)
-values ('36.517.634/0001-63', 'Av. Tancredo Neves, 620, Salvador, BA, Brasil', 'Kontulari',
-        'Kontulari Contabilidade S/A');
-
-insert into empresa (cnpj, endereco, nome_fantasia, razao_social)
-values ('77.222.441/0001-63', 'Av. Nova, 887, Feira de Santana, BA, Brasil', 'bpago', 'Bpago Pagamentos Ltda.');
